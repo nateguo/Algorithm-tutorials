f852e21d21840366839bc8f47b4b9d3955158478
nateguo <boole.z.guo@gmail.com>
2013-10-31 17:32:05 +0800

    Fix simple lcs

--- src/org/test/avl/LowestCommonAncestor.java
+++ src/org/test/avl/LowestCommonAncestor.java
@@ -10,7 +10,10 @@ public class LowestCommonAncestor {
 			return node;
 		}
 		int[] leftFlag, rightFlag;
-		leftFlag = rightFlag = new int[]{0};
+		leftFlag = new int[1];
+		rightFlag = new int[1];
+		leftFlag[0] = 0;
+		rightFlag[0] = 0;
 		if (node.left != null) {
 			AVLNode<T> leftAncestor = lca(node.left, u, v, leftFlag);
 			if (leftAncestor != null)
@@ -36,7 +39,8 @@ public class LowestCommonAncestor {
 	}
 	
 	public static <T extends Comparable<? super T>> AVLNode<T> lca(AVLTree<T> tree, T u, T v) {
-		int[] flag = {0};
+		int[] flag = new int[1];
+		flag[0] = 0;
 		return lca(tree.getRoot(), u, v, flag);
 	}
 	
@@ -52,6 +56,11 @@ public class LowestCommonAncestor {
 		System.out.println ("Prefix Traversal:");
 		System.out.println(tree.serializePrefix());
 		
-		System.out.println("lca:  " + lca(tree, 1, 3).data);
+		System.out.println("lca(1,3) :  " + lca(tree, 1, 3).data);
+		System.out.println("lca(1,5) :  " + lca(tree, 1, 5).data);
+		System.out.println("lca(5,9) :  " + lca(tree, 5, 9).data);
+		System.out.println("lca(2,9) :  " + lca(tree, 2, 9).data);
+		System.out.println("lca(2,6) :  " + lca(tree, 2, 9).data);
+		System.out.println("lca(5,7) :  " + lca(tree, 5, 7).data);
 	}
 }
